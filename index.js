1fb31680fde8099aa1611fe59e0571518add0b6c
shaymarsiano1 <156427178+shaymarsiano1@users.noreply.github.com>
2025-01-06 18:04:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,42 @@ document.querySelectorAll('.like-button').forEach(button => {
     button.addEventListener('click', () => {
         const heartIcon = button.querySelector('i');
 
-        // Toggle between outlined and filled heart
         if (heartIcon.classList.contains('bi-heart')) {
             heartIcon.classList.remove('bi-heart');
             heartIcon.classList.add('bi-heart-fill');
-            heartIcon.style.color = 'red'; // Change to red on click
+            heartIcon.style.color = 'red'; 
         } else {
             heartIcon.classList.remove('bi-heart-fill');
             heartIcon.classList.add('bi-heart');
-            heartIcon.style.color = ''; // Reset to the default color
+            heartIcon.style.color = ''; 
         }
     });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const icons = document.querySelectorAll(".footer_icon");
+
+    icons.forEach(icon => {
+        icon.addEventListener("click", handleIconClick);
+        icon.addEventListener("touchstart", handleIconClick);
+    });
+
+    function handleIconClick(event) {
+        event.preventDefault(); 
+        icons.forEach(i => i.classList.remove("active")); 
+        this.classList.add("active"); 
+
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    }
+});
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const homeIcon = document.querySelector(".home-icon");
+
+    homeIcon.addEventListener("click", (e) => {
+        e.preventDefault(); 
+        window.scrollTo({ top: 0, behavior: "smooth" }); 
+    });
+});
+
